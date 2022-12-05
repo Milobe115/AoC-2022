ce0cc4b78d6cc82e264aa485fb89f04c78f8417d
Milo Politan <milobe.115@gmail.com>
2022-12-05 12:11:49 +0100

    day05

---
+++ day05/star2.py
@@ -0,0 +1,48 @@
+import re
+from dataclasses import dataclass
+
+
+@dataclass
+class Instruction:
+    how_many: int
+    origin: int
+    destination: int
+
+
+def parse_initial_state(lines):
+    nb_stacks = len(lines[-1]) // 4 + 1
+    stacks = [[] for _ in range(nb_stacks)]
+    nb_lines = len(lines)
+    for i in range(nb_lines - 2, -1, -1):
+        for j in range(nb_stacks):
+            if 4 * j + 1 < len(lines[i]) and lines[i][4 * j + 1] != " ":
+                stacks[j].append(lines[i][4 * j + 1])
+                print(f"Pushing {lines[i][4 * j + 1]} to stack {j}")
+    print(stacks)
+    return stacks
+
+
+def parse_instructions(lines):
+    # Compile a regex to parse instructions "Move X from A to B" with X and A and B being integers
+    regex = re.compile(r"move (\d+) from (\d+) to (\d+)")
+    move_instructions = [Instruction(*map(int, regex.match(line).groups())) for line in lines]
+    return move_instructions
+
+
+with open("./input.txt", 'r') as f:
+    data = f.readlines()
+    # Initial state
+    separator = data.index('\n')
+    stacks = parse_initial_state(data[:separator])
+    instructions = parse_instructions(data[separator + 1:])
+    # Apply instructions
+
+    for instruction in instructions:
+        print(f"Moving {instruction.how_many} from {instruction.origin} to {instruction.destination}")
+        print(f"Before : {stacks[instruction.origin - 1]} | {stacks[instruction.destination - 1]}")
+        stacks[instruction.destination - 1].extend(stacks[instruction.origin - 1][-instruction.how_many:])
+        stacks[instruction.origin - 1] = stacks[instruction.origin - 1][:-instruction.how_many]
+        print(f"After  : {stacks[instruction.origin - 1]} | {stacks[instruction.destination - 1]}")
+
+# Print top of stacks
+    print("".join([stack[-1] for stack in stacks]))
